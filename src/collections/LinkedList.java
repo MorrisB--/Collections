ee7b648dcbd9e52de88956a6b476580f78b7b70b
Morris Ballenger <morris.ballenger@yahoo.com>
2016-02-25 14:30:10 -0600

    Fixed EVERYTHING

--- src/collections/LinkedList.java
+++ src/collections/LinkedList.java
@@ -1,98 +1,16 @@
 package collections;
 
-public class LinkedList<T> extends Node<T> implements List<T> {
+public class LinkedList<T> implements List<T> {
 
+	private static int counter;
 	private Node<T> root;
-	private int counter = 0;
 
-	public int size() {
-		/*
-		 * int size =0; Node<> if(this.info == null) return size; if
-		 * (this.next.info == null){ size++; return size; } if(this.next.info !=
-		 * null){ size++; Node<T> temp = new Node<T>(); temp = this.next; }
-		 */
-		return 0;
-	}
-
-	public boolean contains(T val) {
-		return false;
-	}
-
-	public void clear() {
-		Node<T> temp = new Node<T>();
-		root = temp;
-	}
-
-	public T get(int pos) {
-		return null;
-	}
-
-	public T get() {
-		return null;
-	}
-
-	public T remove(int pos) {
-		// should we consider if the user gives a number that is out of bounds?
-		// if (pos < 0 || pos > size())
-		// return null;
-
-		// save the element currently at pos to return to the user
-		T element = get(pos - 1);
-
-		Node<T> current = this.root;
-		if (root != null) {
-			for (int i = 0; i < pos; i++) {
-				if (current.next == null)
-					return element;
-
-				current = current.next;
-			}
-			current.next = current.next.next;
-
-			// decrement the number of elements variable
-			this.counter--;
-			return element;
-
-		}
-		return element;
-	}
-
-	public T remove() {
-		T element = get(size() - 1);
-
-		Node<T> current = this.root;
-		if (root != null)
-			while (current.next.next != null) {
-				current = current.next;
-			}
-		current.next = null;
-		this.counter--;
-		return element;
-	}
-
-	public void add(T info, int pos) {
-
-		Node<T> temp = new Node<T>(info);
-		Node<T> current = root;
-
-		if (current != null)
-			// crawl to the requested index or the last element in the list
-			for (int i = 1; i < pos && current.getNext() != null; i++)
-				current = current.getNext();
-
-		// set the new node's next-node to this node's next-node reference
-		temp.next = current.getNext();
-
-		// now set this node's next-node reference to the new node
-		current.next = temp;
-
-		// increment the number of elements variable
-		this.counter++;
+	public LinkedList() {
 
 	}
 
 	public void add(T info) {
-		// Initialize Node only incase of 1st element
+
 		if (root == null) {
 			root = new Node<T>(info);
 		}
@@ -100,42 +18,152 @@ public class LinkedList<T> extends Node<T> implements List<T> {
 		Node<T> temp = new Node<T>(info);
 		Node<T> current = root;
 
-		// Let's check for NPE before iterate over current
 		if (current != null) {
 
-			/*
-			 * starting at the head node, crawl to the end of the list and then
-			 * add element after last node
-			 */
 			while (current.getNext() != null) {
 				current = current.getNext();
 			}
 
 			// the last node's "next" reference set to our new node
-			current.next = temp;
+			current.setNext(temp);
 		}
 
-		// increment the number of elements variable
-		this.counter++;
+		incrementCounter();
 	}
 
-	public String toString() {
-		String output = "[";
+	private static int getCounter() {
+		return counter;
+	}
 
-		if (root != null) {
-			Node<T> current = root.getNext();
+	private static void incrementCounter() {
+		counter++;
+	}
 
-			while (current != null) {
-				if (current.next == null) {
-					output += current.toString();
-				} else
-					output += current.toString() + ", ";
+	private void decrementCounter() {
+		counter--;
+	}
 
+	public void add(T info, int index) {
+		Node<T> temp = new Node<T>(info);
+		Node<T> current = root;
+
+		if (current != null) {
+			for (int i = 1; i < index && current.getNext() != null; i++) {
 				current = current.getNext();
 			}
 		}
 
-		return output + "]";
+		temp.setNext(current.getNext());
+
+		current.setNext(temp);
+
+		incrementCounter();
+	}
+
+	@SuppressWarnings("unchecked")
+	public T get(int index) {
+		if (index < 0)
+			return null;
+		Node<T> current = null;
+		if (root != null) {
+			current = root.getNext();
+			for (int i = 0; i < index; i++) {
+				if (current.getNext() == null)
+					return null;
+
+				current = current.getNext();
+			}
+			return current.getInfo();
+		}
+
+		// This may break something, not sure why it works
+		return (T) current;
+
+	}
+
+	public T remove(int index) {
+
+		if (index < 0 || index > size())
+			return null;
+
+		Node<T> current = root;
+		Node<T> returnRemoved = new Node<T>(current.getInfo());
+		if (root != null) {
+
+			if (index == 0) {
+				root.setInfo(null);
+				root = root.getNext();
+				return returnRemoved.getInfo();
+			}
+
+			for (int i = 0; i < index; i++) {
+				if (current.getNext() == null)
+					return null;// false;
+
+				current = current.getNext();
+			}
+			// save the info being removed to return at the end
+			returnRemoved.setInfo(current.getInfo());
+			current.setNext(current.getNext().getNext());
+
+			decrementCounter();
+			return returnRemoved.getInfo();
+
+		}
+		return null;
+	}
+
+	public int size() {
+		return getCounter();
+	}
+
+	public String toString() {
+		String output = "";
+
+		if (root != null) {
+			Node<T> current = root.getNext();
+			while (current != null) {
+				output += "[" + current.getInfo().toString() + "]";
+				current = current.getNext();
+			}
+
+		} else
+			output = "[]";
+
+		return output;
+	}
+
+	public T remove() {
+		return remove(getCounter() - 1);
+	}
+
+	public void clear() {
+		Node<T> temp = new Node<T>(null);
+		root = temp;
+	}
+
+	public T get() {
+		return get(size() - 1);
+	}
+
+	public boolean contains(T val) throws NullPointerException {
+		Node<T> current = root;
+		try {
+			int i = 0;
+
+			while (current != null) {
+				if (this.get(i).equals(val)) {
+					return true;
+				} else {
+					current = current.getNext();
+					i++;
+				}
+			}
+			return false;
+		} catch (Exception e) {
+			return false;
+		}
+
 	}
 
 }
--- src/collections/Node.java
+++ src/collections/Node.java
@@ -1,70 +1,34 @@
-
 package collections;
 
-public class Node<T> {
-	T info;
-	Node<T> next;
-	
-	public Node(){
-		
-	}
-	public Node(T value){
-		this.info = value;
-		this.next = null;
-	}
-
-	public String toString() {
-		return "" + info;
-	}
-
-	public static void main(String[] args) {
-		Node<Integer> n = new Node<Integer>();
-		n.info = 92;
-
-		Node<Integer> m = new Node<Integer>();
-		m.info = 94;
-		Node<Integer> o = new Node<Integer>();
-		o.info = 96;
-		Node<Integer> p = new Node<Integer>();
-		p.info = 98;
-		Node<Integer> r = new Node<Integer>();
-		r.info = 100;
-
-		n.next = m;
-		deleteAddr(m);
-		changeValue(m);
-
-		n.next.next = o;
-		o = null;
-		n.next.next.next = p;
-		p = null;
-		n.next.next.next.next = r;
-		r = null;
-		// displayInfo(n);
-		deleteAddr(n);
-
-	}
-
-	//This will only work with integers, figure out a way to have it generic
-	public static void deleteAddr(Node<Integer> x) {
-		x = null;
-	}
-
-	//This will only work with integers, figure out a way to have it generic
-	public static void changeValue(Node<Integer> x) {
-		x.info = 2 * x.info;
-	}
-
-	//This will only work with integers, figure out a way to have it generic
-	public static void displayInfo(Node<Integer> r) {
-		while (r != null) {
-			System.out.print(r.info + " ");
-			r = r.next;
+	final class Node<T> {
+		Node<T> next;
+		T info;
+ 
+		public Node(T infoValue) {
+			next = null;
+			info = infoValue;
 		}
-		System.out.println();
+ 
+
+		public Node(T infoValue, Node<T> nextValue) {
+			next = nextValue;
+			info = infoValue;
+		}
+ 
+		public T getInfo() {
+			return info;
+		}
+ 
+		public void setInfo(T infoValue) {
+			info = infoValue;
+		}
+ 
+		public Node<T> getNext() {
+			return next;
+		}
+ 
+		public void setNext(Node<T> nextValue) {
+			next = nextValue;
+		}
+ 
 	}
-	
-	public Node<T> getNext(){
-		return next;
-	}
-}
