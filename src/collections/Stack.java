2a6bffaf1e3ee0fff7b923535eeb47f8aa9afae0
MorrisB-- <morris.ballenger@yahoo.com>
2016-04-03 16:44:03 -0500

    Added Stack.java

---
+++ src/collections/Stack.java
@@ -0,0 +1,28 @@
+package collections;
+
+public class Stack<T> {
+
+	private Node<T> root;
+	private int size = 0;
+
+	public void push(T info) {
+
+	}
+
+	public T pop() {
+		return null;
+	}
+
+	public T peek() {
+		return null;
+	}
+
+	public int size() {
+		return this.size;
+	}
+	
+	public boolean isEmpty(){
+		return false;
+	}
+
+}
