7ffdfc70a2c19382cb5fe06b562543213adaad8d
MorrisB-- <morris.ballenger@yahoo.com>
2016-04-03 15:30:04 -0500

    Updated comments.

--- src/collections/LinkedList.java
+++ src/collections/LinkedList.java
@@ -1,4 +1,4 @@
-package collections; 
+package collections;
 
 public class LinkedList<T> implements List<T> {
 
@@ -83,49 +83,13 @@ public class LinkedList<T> implements List<T> {
 
 	}
 
-	/*public T remove(int index) throws NullPointerException {
-
-		if (index < 1 || index > size())
-			return null;
-	//	if (index > 0)
-	//		index--;
-	////	else
-	//		index = 0;
-		Node<T> current = root;
-		Node<T> returnRemoved = new Node<T>(current.getInfo());
-		if (root != null) {
-
-			if (index == 0) {
-				root.setInfo(null);
-				root = root.getNext();
-				return returnRemoved.getInfo();
-			}
-
-			for (int i = 0; i < index-1; i++) {
-				if (current.getNext() == null) {
-					return null;// false;
-				}
-				System.out.println("Entered " + i);
-				current = current.getNext();
-			}
-			// save the info being removed to return at the end
-			returnRemoved.setInfo(current.getInfo());
-			current.setNext(current.getNext().getNext());
-
-			decrementCounter();
-			return returnRemoved.getInfo();
-
-		}
-		return null;
-	}*/
-	
-	public T remove(int index){
+	public T remove(int index) {
 		Node<T> current = root;
 
 		if (index == 0)
-			root=root.getNext();
-		else{
-			for (int i = 0; i<index-1;i++){
+			root = root.getNext();
+		else {
+			for (int i = 0; i < index - 1; i++) {
 				current = current.next;
 			}
 			current.setNext(current.getNext().getNext());
@@ -133,28 +97,26 @@ public class LinkedList<T> implements List<T> {
 		return null;
 	}
 
-
 	public int size() {
 		return getCounter();
 	}
 
-	public String toString() throws NullPointerException{
+	public String toString() throws NullPointerException {
 		String output = "";
 
-		try{
+		try {
 
-		if (root != null) {
-			Node<T> current = root;
-		//	output += root.getInfo().toString();
-			while (current != null) {
-				output += "[" + current.getInfo().toString() + "]";
-				current = current.getNext();
-			}
+			if (root != null) {
+				Node<T> current = root;
+				// output += root.getInfo().toString();
+				while (current != null) {
+					output += "[" + current.getInfo().toString() + "]";
+					current = current.getNext();
+				}
 
-		} else
-			output = "[]";
-		}
-		catch(Exception e){
+			} else
+				output = "[]";
+		} catch (Exception e) {
 			System.out.println("[]");
 		}
 
